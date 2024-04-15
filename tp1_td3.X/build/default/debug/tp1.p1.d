d7092c3f34e6383350404257a55e0b714d295eaf
pauaguilera8 <85347@electronica.frc.utn.edu.ar>
2024-04-15 13:51:35 -0300

    creacion

---
+++ tp1_td3.X/build/default/debug/tp1.p1.d
@@ -0,0 +1,2 @@
+build/default/debug/tp1.p1:  \
+tp1.c 
---
+++ tp1_td3.X/tp1.c
@@ -0,0 +1,31 @@
+
+// PIC12F629 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSC oscillator: I/O function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-Up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // GP3/MCLR pin function select (GP3/MCLR pin function is digital I/O, MCLR internally tied to VDD)
+#pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
+#pragma config CP = OFF         // Code Protection bit (Program Memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+int num_a;
+int num_b;
+void main(void) {
+    
+    while(1){
+        num_a += 1;
+        
+        if(num_a == 10){
+            num_a=0;
+        }
+    }
+    return;
+}
